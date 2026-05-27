1c7eedd4b4d49570399e53dfe62be2b4c0613e83
Njabulo Moshifa <njmosjhb025@student.wethinkcode.co.za>
2026-05-27 16:53:54 +0200

    Finished the getting student

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 import sqlite3
 import argparse
 import os
